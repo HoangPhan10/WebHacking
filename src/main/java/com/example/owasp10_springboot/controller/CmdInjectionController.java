87613d65128e0a51efb7e49e0fabb551bb4dfbc5
phanhoang10 <phanhoang1022002@gmail.com>
2023-04-12 06:40:41 -0400

    update command injection

--- src/main/java/com/example/owasp10_springboot/controller/CmdInjectionController.java
+++ src/main/java/com/example/owasp10_springboot/controller/CmdInjectionController.java
@@ -32,13 +32,20 @@ public class CmdInjectionController {
         model.addAttribute("CmdDTO",new CmdDTO(""));
         return "commandInjection/lab1/index";
     }
+    @GetMapping("/command-injection/lab2")
+    public String getCommandInjectionLab2(Model model){
+        model.addAttribute("CmdDTO",new CmdDTO(""));
+        return "commandInjection/lab2/index";
+    }
     @PostMapping(path = "/command-injection/lab1")
     public String postCommandInjectionLab1(CmdDTO cmdDTO,Model model){
         model.addAttribute("CmdDTO",new CmdDTO(""));
-        if (!cmdDTO.getCmd().startsWith("127.0.0.1")){
+        String regex = "^[a-zA-Z0-9 .;>/]*$";
+        if (!cmdDTO.getCmd().startsWith("127.0.0.1")|!cmdDTO.getCmd().matches(regex)){
             model.addAttribute("error","PING Error");
             return "commandInjection/lab1/index";
         }
+
         List<String> results = implCMDInjectionService.runShell(cmdDTO.getCmd());
         for(String  str : results){
             log.info(str);
@@ -46,7 +53,24 @@ public class CmdInjectionController {
         model.addAttribute("success","PING Successfully");
         return "commandInjection/lab1/index";
     }
+    @PostMapping(path = "/command-injection/lab2")
+    public String postCommandInjectionLab2(CmdDTO cmdDTO,Model model){
+        log.info(cmdDTO.getCmd());
+        model.addAttribute("CmdDTO",new CmdDTO(""));
+        String regex = "^[a-z-A-Z0-9 .:`,=;/]*$";
+        System.out.print(cmdDTO.getCmd().matches(regex));
+        if (!cmdDTO.getCmd().startsWith("127.0.0.1")|!cmdDTO.getCmd().matches(regex)){
+            model.addAttribute("error","PING Error");
+            return "commandInjection/lab2/index";
+        }
 
+        List<String> results = implCMDInjectionService.runShell(cmdDTO.getCmd());
+        for(String  str : results){
+            log.info(str);
+        }
+        model.addAttribute("success","PING Successfully");
+        return "commandInjection/lab2/index";
+    }
     @GetMapping("/")
     public String getHome(){
         return "index/index";
@@ -54,10 +78,13 @@ public class CmdInjectionController {
     @GetMapping("/images")
     public void getImage(@RequestParam("filename") String filename, HttpServletResponse response) {
         try {
-            File file = new File("/var/www/images/" + filename);
-            InputStream is = new FileInputStream(file);
-            IOUtils.copy(is, response.getOutputStream());
-            response.flushBuffer();
+            String regex = "^.+(\\.jpeg|\\.jpg|\\.txt)$";
+            if (filename.matches(regex)){
+                File file = new File("/var/www/images/" + filename);
+                InputStream is = new FileInputStream(file);
+                IOUtils.copy(is, response.getOutputStream());
+                response.flushBuffer();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
